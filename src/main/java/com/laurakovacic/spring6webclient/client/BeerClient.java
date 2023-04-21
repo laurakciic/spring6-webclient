9b02892d1b5b9d2a5ef7dcede3350bf39decc162
Laura <laurakovacic@outlook.com>
2023-04-21 20:31:25 +0200

    get list as Java Map

--- src/main/java/com/laurakovacic/spring6webclient/client/BeerClient.java
+++ src/main/java/com/laurakovacic/spring6webclient/client/BeerClient.java
@@ -2,7 +2,11 @@ package com.laurakovacic.spring6webclient.client;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Map;
+
 public interface BeerClient {
 
     Flux<String> listBeers();
+
+    Flux<Map> listBeersMap();
 }
--- src/test/java/com/laurakovacic/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/com/laurakovacic/spring6webclient/client/BeerClientImplTest.java
@@ -1,10 +1,14 @@
 package com.laurakovacic.spring6webclient.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -12,14 +16,30 @@ class BeerClientImplTest {
     @Autowired
     BeerClient client;
 
+    public AtomicBoolean atomicBoolean;
+
+    @BeforeEach
+    void setUp() {
+        atomicBoolean = new AtomicBoolean(false);
+    }
+
     @Test
     void listBeers() {
-        client.listBeers().subscribe(System.out::println);
+        client.listBeers().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void listBeersMap() {
+        client.listBeersMap().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
     }
 }
